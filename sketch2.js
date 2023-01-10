7c73846e92d45fb1c9dc031da12cc232b057ef35
gustavocambareri <gustavo.cambareri@gmail.com>
2023-01-10 13:39:58 +0100

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,9 +1,33 @@
+let mic;
+var volhistory = [];
+let binaryX = 0;
+let binaryY = 0;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  let canvass = createCanvas(windowWidth, windowHeight);
+  canvass.mousePressed(userStartAudio);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
-  square(20, 20, 20);
+  // background(0);
+  stroke(255, 0, 0);
+  strokeWeight(2);
+  // divide screen in 3
+  line(0, height / 3, width, height / 3);
+  line(0, height / 1.5, width, height / 1.5);
+
+  micLevel = mic.getLevel();
+  let x = height - micLevel * height;
+  ellipse(x, height / 2, 10, 10);
+
+  volhistory.push(micLevel);
+
+  if (frameCount % 20 === 0) {
+    binaryY += +12;
+  }
+  text(volhistory, binaryX, binaryY);
+
+  console.log(frameCount);
 }
