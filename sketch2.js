f107aa1978072993a2434067ad91073232434f94
gustavocambareri <gustavo.cambareri@gmail.com>
2023-01-12 15:55:34 +0100

    LA SVOLTA

--- sketch2.js
+++ sketch2.js
@@ -2,6 +2,10 @@ let mic;
 var volhistory = [];
 let binaryX = 0;
 let binaryY = 0;
+let rectColor = [];
+let rectColorValueA;
+let rectColorValueB;
+let rectColorValueC;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,15 +17,14 @@ function setup() {
 }
 
 function draw() {
-  
-  
-
+  background(255);
   stroke(0, 255, 0);
   strokeWeight(1);
   // divide screen in 3
   line(0, height / 3, width, height / 3);
   line(0, height / 1.5, width, height / 1.5);
 
+  // parte 1
   micLevel = mic.getLevel();
   let x = height - micLevel * height;
   ellipse(x, height / 2, 10, 10);
@@ -31,7 +34,21 @@ function draw() {
   if (frameCount % 20 === 0) {
     binaryY += +12;
   }
-  text(volhistory, binaryX, binaryY);
+  // text(volhistory, binaryX, binaryY);
 
-  console.log(frameCount);
+  //parte 3
+  newmicLevelA = map(micLevel, 0, 1, 0, 255);
+  newmicLevelB = map(micLevel, 0, 1, 0, 0);
+  newmicLevelC = map(micLevel, 0, 1, 255, 0);
+  text(nfc(newmicLevelA, 4), 10, 30);
+
+  rectColorValueA = newmicLevelA;
+  rectColorValueB = newmicLevelB;
+  rectColorValueC = newmicLevelC;
+
+  fill(rectColorValueA, rectColorValueB, rectColorValueC);
+  rect(0, height / 1.5, width, height / 1.5);
+  console.log(rectColorValueA);
+  console.log(rectColorValueB);
+  console.log(rectColorValueC);
 }
