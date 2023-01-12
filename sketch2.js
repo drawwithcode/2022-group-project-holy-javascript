9adbc3f2b9783e8e4f3129e1b4f9a9e9dd937771
Lucapedro99 <luca1.pedroni@mail.polimi.it>
2023-01-12 14:27:29 +0100

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,13 +1,37 @@
-var mic;
+let mic;
+var volhistory = [];
+let binaryX = 0;
+let binaryY = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  mic = new p5.AudioIn();
-  mic.start
   // put setup code here
+  let canvass = createCanvas(windowWidth, windowHeight);
+  canvass.mousePressed(userStartAudio);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
-  var vol = mic.getlevel()
+  
+  
+
+  stroke(0, 255, 0);
+  strokeWeight(1);
+  // divide screen in 3
+  line(0, height / 3, width, height / 3);
+  line(0, height / 1.5, width, height / 1.5);
+
+  micLevel = mic.getLevel();
+  let x = height - micLevel * height;
+  ellipse(x, height / 2, 10, 10);
+
+  volhistory.push(micLevel);
+
+  if (frameCount % 20 === 0) {
+    binaryY += +12;
+  }
+  text(volhistory, binaryX, binaryY);
+
+  console.log(frameCount);
 }
