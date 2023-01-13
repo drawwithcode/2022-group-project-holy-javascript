3702596b6b974ddc015aef79c43b1c8a2afb325c
gustavocambareri <gustavo.cambareri@gmail.com>
2023-01-13 16:04:57 +0100

    ORDINE

--- sketch2.js
+++ sketch2.js
@@ -21,35 +21,33 @@ function draw() {
   background(255);
   stroke(0, 255, 0);
   strokeWeight(1);
+
+  //avvia microfono
+  micLevel = mic.getLevel();
+
   // divide screen in 3
   line(0, height / 3, width, height / 3);
   line(0, height / 1.5, width, height / 1.5);
 
-  // parte 1
-  micLevel = mic.getLevel();
-  let x = height - micLevel * height;
-  ellipse(x, height / 2, 10, 10);
-
-  volhistory.push(micLevel);
-
-  if (frameCount % 20 === 0) {
-    binaryY += +12;
-  }
-  // text(volhistory, binaryX, binaryY);
-
-  //parte 3
+  //parte 1
   newmicLevelA = map(micLevel, 0, 1, 0, 255);
   newmicLevelB = map(micLevel, 0, 1, 0, 0);
   newmicLevelC = map(micLevel, 0, 1, 255, 0);
   textSize(100);
   text(nfc(newmicLevelA, 4), width / 2, height / 5);
 
+  // parte 2
+  let x = height - micLevel * height;
+  ellipse(x, height / 2, 10, 10);
+
+  //parte 3
   rectColorValueA = newmicLevelA;
   rectColorValueB = newmicLevelB;
   rectColorValueC = newmicLevelC;
 
   fill(rectColorValueA, rectColorValueB, rectColorValueC);
   rect(0, height / 1.5, width, height / 1.5);
+
   console.log(rectColorValueA);
   console.log(rectColorValueB);
   console.log(rectColorValueC);
